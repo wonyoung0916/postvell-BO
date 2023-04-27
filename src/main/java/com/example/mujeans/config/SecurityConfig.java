ecbd317617287578fbf944c78f77c1fc18042dcc
김지수 <jskim@idevel.kr>
2023-04-28 05:30:12 +0900

    시큐리티 설정 롤백

--- src/main/java/com/example/mujeans/config/SecurityConfig.java
+++ src/main/java/com/example/mujeans/config/SecurityConfig.java
@@ -17,20 +17,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http
                 .cors().disable()		//cors방지
                 .csrf().disable()		//csrf방지
-                .headers().frameOptions().disable()
-                .and()
-            .authorizeRequests()
-                .antMatchers("/board/MyList", "/board/MyDetail").authenticated()// 로그인이 필요한 페이지는 인증된 사용자만 접근 가능
-                .anyRequest().permitAll()  // 로그인 페이지와 회원가입 페이지는 인증 없이 접근 가능
-                .and()
-            .formLogin()
-                .loginPage("/login/index") // 로그인 페이지 경로 설정
-                .defaultSuccessUrl("/") // 로그인 성공 시 리다이렉트할 페이지 경로 설정
-                .and()
-            .logout()
-                .logoutUrl("/login/logoutProc") // 로그아웃 URL 설정
-                .logoutSuccessUrl("/login/index") // 로그아웃 성공 시 리다이렉트할 페이지 경로 설정
-                .invalidateHttpSession(true);
+                .headers().frameOptions().disable();
     }
 
     @Bean
