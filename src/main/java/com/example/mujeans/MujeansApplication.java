724ab822814528d2954a5e790b073fbe489534e8
김지수 <jskim@idevel.kr>
2023-04-27 14:33:42 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/mujeans/MujeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.mujeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MujeansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mujeans/MujeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mujeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
